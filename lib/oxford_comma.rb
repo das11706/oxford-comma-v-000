2c66ba53d29611404eef0ffd0e47a13bfbfc6b73
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-18 02:22:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ elsif array.length > 3
   array.each do |fruit|
   #array.join(", ")
     if array.last
-    array.replace(array << "and #{fruit}")
+    array.replace("and #{fruit}")
     #array << "and #{fruit}"
     end
   end
